bc88723b12214d3d4c7a7d050174934f938406fa
decio88 <decio88@users.noreply.github.com>
2023-02-05 11:11:01 +0100

    refactored the code and added task adding features

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './style.css';
 import Task from './modules/task';
-import renderHome from './modules/home';
 
 // create a task from the form that will appear as overlay on the page when clicking new task button
-renderHome();
